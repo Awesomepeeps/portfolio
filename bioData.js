9a917617004da36963b9008709284b38ad7ca18c
RiseSky <67874744+Awesomepeeps@users.noreply.github.com>
2025-04-19 23:01:08 -0700

    Update bioData.js

--- bioData.js
+++ bioData.js
@@ -1,6 +1,6 @@
 // bioData.js
 const bioData = {
-  bio: `High school sophomore and aspiring aerospace engineer Nikaansh Singh earned the NASA SLI Payload Design Award for his innovative rocket design featuring a dynamic aerobrake system. His achievements with Portland Rocketry and participation in key competitions showcase both technical expertise and leadership.`,
+  bio: `High school junior and aspiring aerospace engineer Nikaansh Singh earned the NASA SLI Payload Design Award for his innovative rocket design featuring a dynamic aerobrake system. His achievements with Portland Rocketry and participation in key competitions showcase both technical expertise and leadership.`,
   resumeLink:
     "https://docs.google.com/document/d/1xOpIpcQ4YZEk0xlyqJd2V3STzV4u0RQjVtV2C9b-JbU/edit?usp=sharing",
   socials: {
