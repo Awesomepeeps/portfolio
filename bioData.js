e11df5639050c56276435e5ec4f21888f3656f43
Nikaansh Singh <67874744+Awesomepeeps@users.noreply.github.com>
2025-06-04 22:45:31 -0700

    Update bioData.js

--- bioData.js
+++ bioData.js
@@ -2,7 +2,7 @@
 const bioData = {
   bio: `High school junior and aspiring aerospace engineer Nikaansh Singh earned the NASA SLI Payload Design Award for his innovative rocket design featuring a dynamic aerobrake system. His achievements with Portland Rocketry and participation in key competitions showcase both technical expertise and leadership.`,
   resumeLink:
-    "https://docs.google.com/document/d/1xOpIpcQ4YZEk0xlyqJd2V3STzV4u0RQjVtV2C9b-JbU/edit?usp=sharing",
+    "https://docs.google.com/document/d/1lFhPLDQCPOC0y20MJeOcfxIQxmtp2HIAnRmeR0EKNLc/edit?usp=sharing",
   socials: {
     github: "https://github.com/Awesomepeeps",
     linkedin: "https://www.linkedin.com/in/nikaansh-singh/",
